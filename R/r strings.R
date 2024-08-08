08d63178292c0b24f4bf39a8db2b3614e9fca44a
SaisriDBN <bhargavisiri36@gmail.com>
2024-08-08 13:26:31 +0530

    r strings.R

--- R/r strings.R
+++ R/r strings.R
@@ -1,1 +1,13 @@
+#print string
+str("R is awesome")
+str
+
+# Numbers of characters in the string
+str("R is awesome")
+nchar(str)
+
+#Check a string
+str("R is awesome")
+grepl("apple", str)
+grepl("is", str)
 
