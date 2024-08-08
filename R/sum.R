b81e679dcafaf72e2c9addc325e1b0f15e7a0d60
SaisriDBN <bhargavisiri36@gmail.com>
2024-08-08 13:19:46 +0530

    r maths.R

--- R/sum.R
+++ R/sum.R
@@ -5,3 +5,13 @@ print(result)
 #subtraction
 sub <- 10-5
 sub  #print the result
+
+#Square root
+sqrt(625)
+
+#absolute postive value
+abs(-4.67)
+
+#ceiling and floor
+ceiiling(9.567)
+floor(6.78)
